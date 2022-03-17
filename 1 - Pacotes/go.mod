89dd45de352e7af41661fa404c9f823a3b61942c
Luciano Ortiz <lucianoortizsilva@gmail.com>
2022-03-16 21:18:11 -0300

    Importando pacote externo

--- 1 - Pacotes/go.mod
+++ 1 - Pacotes/go.mod
@@ -1,3 +1,5 @@
 module modulo
 
 go 1.18
+// importando pacote externo: https://github.com/badoux/checkmail
+require github.com/badoux/checkmail v1.2.1
--- 1 - Pacotes/main.go
+++ 1 - Pacotes/main.go
@@ -4,9 +4,13 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Escrevendo no arquivo main")
 	auxiliar.Escrever()
+	erro := checkmail.ValidateFormat("lucianoortizsilva@gmail.com")
+	fmt.Println(erro)
 }
