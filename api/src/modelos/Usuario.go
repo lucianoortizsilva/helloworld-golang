226b3372ea0ce2166a01b516cee92b71163de76b
Luciano Ortiz <lucianoortizsilva@gmail.com>
2022-04-04 17:01:22 -0300

    Add validacoes para model Usuario

--- api/src/modelos/Usuario.go
+++ api/src/modelos/Usuario.go
@@ -1,6 +1,10 @@
 package modelos
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 // Usuario representa um usuário utilizando a rede social
 type Usuario struct {
@@ -10,4 +14,40 @@ type Usuario struct {
 	Email    string    `json:"email,omitempty"`
 	Senha    string    `json:"senha,omitempty"`
 	CriadoEm time.Time `json:"criadoEm,omitempty"`
+}
+
+// Preparar IRÁ VALIDAR E FORMATAR O USUARIO RECEBIDO
+func (u *Usuario) Preparar() error {
+	if erro := u.validar(); erro != nil {
+		return erro
+	}
+	u.formatar()
+	return nil
+}
+
+func (u *Usuario) validar() error {
+
+	if u.Nome == "" {
+		return errors.New("o nome é obrigatório e não pode estar em branco")
+	}
+
+	if u.Nick == "" {
+		return errors.New("o nick é obrigatório e não pode estar em branco")
+	}
+
+	if u.Email == "" {
+		return errors.New("o email é obrigatório e não pode estar em branco")
+	}
+
+	if u.Senha == "" {
+		return errors.New("a senha é obrigatória e não pode estar em branco")
+	}
+
+	return nil
+}
+
+func (u *Usuario) formatar() {
+	u.Nome = strings.TrimSpace(u.Nome)
+	u.Nick = strings.TrimSpace(u.Nick)
+	u.Email = strings.TrimSpace(u.Email)
 }
