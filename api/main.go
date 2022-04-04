fea8c93ffc867f18ab061cc5660222d4815bd77c
Luciano Ortiz <lucianoortizsilva@gmail.com>
2022-04-04 14:41:30 -0300

    Conectando com banco MYSQL

--- api/main.go
+++ api/main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	config.Carregar()
-	fmt.Println("Rodando API")
+	fmt.Printf("Escutando na porta: %d", config.Porta)
 	r := router.Gerar()
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
 
