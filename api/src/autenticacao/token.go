dbf67c172cd70b7f43d72a13f9a7406fa9f4f7bc
Luciano Ortiz <lucianoortizsilva@gmail.com>
2022-04-07 14:31:18 -0300

    Add validação de TOKEN

--- api/src/autenticacao/token.go
+++ api/src/autenticacao/token.go
@@ -2,6 +2,9 @@ package autenticacao
 
 import (
 	"api/src/config"
+	"fmt"
+	"net/http"
+	"strings"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
@@ -14,4 +17,29 @@ func CriarToken(usuarioID uint64) (string, error) {
 	permissoes["usuarioId"] = usuarioID
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, permissoes)
 	return token.SignedString([]byte(config.SecretKey)) //secret
+}
+
+func ValidarToken(r *http.Request) error {
+	tokenTexto := extrairToken(r)
+	token, erro := jwt.Parse(tokenTexto, retornarChaveDeVerificacao)
+	if erro != nil {
+		return erro
+	}
+	fmt.Println(token)
+	return nil
+}
+
+func retornarChaveDeVerificacao(token *jwt.Token) (interface{}, error) {
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return nil, fmt.Errorf("método de assinatura inesperado! %v", token.Header["alg"])
+	}
+	return config.SecretKey, nil
+}
+
+func extrairToken(r *http.Request) string {
+	token := r.Header.Get("Authorization")
+	if len(strings.Split(token, " ")) == 2 {
+		return strings.Split(token, " ")[1]
+	}
+	return ""
 }
