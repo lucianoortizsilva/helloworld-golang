170f0874787aba7170c7d46cbcaa5bc460b9a8d4
Luciano Ortiz <lucianoortizsilva@gmail.com>
2022-04-08 09:37:04 -0300

    Add endpoint /usuarios/{id}/parar-de-seguir

--- api/src/controllers/usuarios.go
+++ api/src/controllers/usuarios.go
@@ -119,7 +119,7 @@ func AtualizarUsuario(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if usuarioID != usuarioIDNoToken {
-		respostas.Erro(w, http.StatusForbidden, errors.New("Não é possível atualizar dados de outro usuário"))
+		respostas.Erro(w, http.StatusForbidden, errors.New("não é possível atualizar dados de outro usuário"))
 		return
 	}
 
@@ -225,6 +225,44 @@ func SeguirUsuario(w http.ResponseWriter, r *http.Request) {
 
 	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
 	if erro = repositorio.Seguir(usuarioID, seguidorID); erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusNoContent, nil)
+
+}
+
+func PararDeSeguirUsuario(w http.ResponseWriter, r *http.Request) {
+
+	seguidorID, erro := autenticacao.ExtrairUsuarioID(r)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
+
+	parametros := mux.Vars(r)
+
+	usuarioID, erro := strconv.ParseUint(parametros["usuarioId"], 10, 64)
+	if erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	if seguidorID == usuarioID {
+		respostas.Erro(w, http.StatusForbidden, errors.New("não é possível parar de seguir você mesmo"))
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
+	if erro = repositorio.PararDeSeguir(usuarioID, seguidorID); erro != nil {
 		respostas.Erro(w, http.StatusInternalServerError, erro)
 		return
 	}
--- api/src/repositorios/usuarios.go
+++ api/src/repositorios/usuarios.go
@@ -149,3 +149,16 @@ func (repositorio Usuarios) Seguir(usuarioID uint64, seguidorID uint64) error {
 	}
 	return nil
 }
+
+func (repositorio Usuarios) PararDeSeguir(usuarioID uint64, seguidorID uint64) error {
+	sql := "delete from seguidores where usuario_id = ? and seguidor_id = ?"
+	statement, erro := repositorio.db.Prepare(sql)
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+	if _, erro = statement.Exec(usuarioID, seguidorID); erro != nil {
+		return erro
+	}
+	return nil
+}
