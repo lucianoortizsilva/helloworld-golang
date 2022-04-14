304f3030479e371de7e774c6e24e642913a6f41c
Luciano Ortiz <lucianoortizsilva@gmail.com>
2022-04-14 11:26:15 -0300

    atualização de script SQL

--- api/sql/dados.sql
+++ api/sql/dados.sql
@@ -1,7 +1,7 @@
 insert into usuarios (nome, nick, email, senha)
 values
 ("Ronaldo", "Fenômeno", "ronaldo@gmail.com", "$2a$10$0iGYlKCAYTyJV/vC6nLGgeWFwD6AhSkWLsVRO/.M4lNK8OtIkfggy"), -- usuario1
-("Ronaldinho", "Faúcho", "ronaldinho@gmail.com", "$2a$10$0iGYlKCAYTyJV/vC6nLGgeWFwD6AhSkWLsVRO/.M4lNK8OtIkfggy"), -- usuario2
+("Ronaldinho", "Gaúcho", "ronaldinho@gmail.com", "$2a$10$0iGYlKCAYTyJV/vC6nLGgeWFwD6AhSkWLsVRO/.M4lNK8OtIkfggy"), -- usuario2
 ("Pelé", "Rei", "pele@gmail.com", "$2a$10$0iGYlKCAYTyJV/vC6nLGgeWFwD6AhSkWLsVRO/.M4lNK8OtIkfggy"); -- usuario3
 
 insert into seguidores(usuario_id, seguidor_id)
