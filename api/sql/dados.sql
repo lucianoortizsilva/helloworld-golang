529a6deda44aa7947cd3cc88483deb45c16e16c7
Luciano Ortiz <lucianoortizsilva@gmail.com>
2022-04-14 10:45:20 -0300

    atualizando script sql com users fakes

--- api/sql/dados.sql
+++ api/sql/dados.sql
@@ -1,8 +1,8 @@
 insert into usuarios (nome, nick, email, senha)
 values
-("Batman", "usuario_1", "batman@gmail.com", "$2a$10$0iGYlKCAYTyJV/vC6nLGgeWFwD6AhSkWLsVRO/.M4lNK8OtIkfggy"), -- usuario1
-("Koringa", "usuario_2", "koringa@gmail.com", "$2a$10$0iGYlKCAYTyJV/vC6nLGgeWFwD6AhSkWLsVRO/.M4lNK8OtIkfggy"), -- usuario2
-("Robin", "usuario_3", "robin@gmail.com", "$2a$10$0iGYlKCAYTyJV/vC6nLGgeWFwD6AhSkWLsVRO/.M4lNK8OtIkfggy"); -- usuario3
+("Ronaldo", "Fenômeno", "ronaldo@gmail.com", "$2a$10$0iGYlKCAYTyJV/vC6nLGgeWFwD6AhSkWLsVRO/.M4lNK8OtIkfggy"), -- usuario1
+("Ronaldinho", "Faúcho", "ronaldinho@gmail.com", "$2a$10$0iGYlKCAYTyJV/vC6nLGgeWFwD6AhSkWLsVRO/.M4lNK8OtIkfggy"), -- usuario2
+("Pelé", "Rei", "pele@gmail.com", "$2a$10$0iGYlKCAYTyJV/vC6nLGgeWFwD6AhSkWLsVRO/.M4lNK8OtIkfggy"); -- usuario3
 
 insert into seguidores(usuario_id, seguidor_id)
 values
@@ -12,6 +12,6 @@ values
 
 insert into publicacoes(titulo, conteudo, autor_id)
 values
-("Publicação do Usuário 1", "Essa é a publicação do usuário 1! Oba!", 1),
-("Publicação do Usuário 2", "Essa é a publicação do usuário 2! Oba!", 2),
-("Publicação do Usuário 3", "Essa é a publicação do usuário 3! Oba!", 3);
+("Ronaldo Nazário", "Eu sou o Ronaldo!", 1),
+("Ronaldinho Gaúcho", "Eu sou Ronaldinho Gaúcho!", 2),
+("Pelé", "Eu sou Pelé! Oba!", 3);
